938d3cfcd4216d31f0ae9bbb74215bf4740cc04c
sakalypse <kevin.limacher@gmail.com>
2021-11-26 18:56:32 +0100

    conf deploy

--- back/src/main.ts
+++ back/src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   require('dotenv').config();
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
